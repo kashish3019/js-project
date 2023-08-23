d68d4b7b886740e786ab9d3b083d8c90f9422ba6
kashish <kashishpatel5253@gmail.com>
2023-08-23 14:25:11 +0530

    cart page

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ const nav = () =>{
     <li><a href="/index.html">home</a></li>
     <li><a href="/pages/product.html">Product</a></li>
     <li><a href="/pages/cart.html">Cart</a></li>
-    <li><a href="/pages/signin.html">Sign In</a></li>
+    <li><a href="/pages/signin.html">Log In</a></li>
     <li><a href="/pages/signup.html">Sign Up</a></li>
     </div>`
 }
--- javascript/product.js
+++ javascript/product.js
@@ -1,54 +1,99 @@
 import nav from "../components/nav.js";
-document.getElementById("navbar").innerHTML=nav()
+document.getElementById("navbar").innerHTML = nav();
 
-const display=(data)=>{
-    data.map((item)=>{
-        let image=document.createElement("img")
-        image.src=item.image
+const display = (data) => {
+  const box2 = document.getElementById("box2");
+  box2.innerHTML = ""; // Clear the existing content
 
-        let title=document.createElement("h3");
-        title.innerHTML=item.title
+  data.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("product-box");
 
-        let price=document.createElement("h3");
-        price.innerHTML=item.price
+    const image = document.createElement("img");
+    image.src = item.image;
 
-        let category=document.createElement("h3");
-        category.innerHTML=item.category
+    const title = document.createElement("h3");
+    title.textContent = item.title;
 
-        let rating=document.createElement("h4");
+    const price = document.createElement("h3");
+    price.textContent = `${item.price}$`;
 
-        if(item.rating.rate>4)
-        {
-            rating.innerHTML="* * * * *"
-            rating.style.color="green"
+    const category = document.createElement("h3");
+    category.textContent = item.category;
+
+    const rating = document.createElement("h4");
+    rating.textContent = "* ".repeat(item.rating.rate);
+
+    const btn1 = document.createElement("button");
+    btn1.textContent = "Add to Cart";
+    btn1.classList.add("btn1");
+
+    div.append(image, title, price, category, rating, btn1);
+    box2.appendChild(div);
+
+    btn1.addEventListener("click", () => {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      let exists = false;
+
+      cart.forEach((cartItem, index) => {
+        if (cartItem.id === item.id) {
+          cart[index].qty += 1;
+          exists = true;
         }
-        else if(item.rating.rate<=4 && item.rating.rate>=3)
-        {
-            rating.innerHTML="* * * *"
-            rating.style.color="chocolate"   
-        }
-        else{
-            rating.innerHTML="* *"
-            rating.style.color="red" 
-        }
+      });
 
-        let btn=document.createElement("button")
-        btn.innerHTML="Buy Now"
+      if (!exists) {
+        cart.push({ ...item, qty: 1 });
+        alert("Added to cart");
+      }
 
-        let btn2=document.createElement("button")
-        btn2.innerHTML="Add to cart"
-
-        let div=document.createElement("div")
-        div.append(image,title,price,category,rating,btn,btn2)
-        document.getElementById("box2").append(div)
-    })
-}
-
-const get=()=>{
-    fetch("http://localhost:3000/products")
-    .then((response)=>response.json())
-    .then((response)=>{
-        display(response)
-    })
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+  });
 };
-get()
+
+let products = [];
+
+document.getElementById("lth").addEventListener("click", () => {
+  products.sort((a, b) => a.price - b.price);
+  display(products);
+});
+
+document.getElementById("htl").addEventListener("click", () => {
+  products.sort((a, b) => b.price - a.price);
+  display(products);
+});
+
+document.getElementById("man").addEventListener("click", () => {
+  const temp = products.filter((val) => val.category === "men's clothing");
+  display(temp);
+});
+
+document.getElementById("woman").addEventListener("click", () => {
+  const temp = products.filter((val) => val.category === "women's clothing");
+  display(temp);
+});
+
+document.getElementById("electronics").addEventListener("click", () => {
+  const temp = products.filter((val) => val.category === "electronics");
+  display(temp);
+});
+
+fetch("http://localhost:3000/products")
+  .then((response) => response.json())
+  .then((data) => {
+    products = data;
+    display(data);
+  });
+
+// Function to fetch and display products initially
+const getProducts = () => {
+  fetch("http://localhost:3000/products")
+    .then((response) => response.json())
+    .then((data) => {
+      products = data;
+      display(data);
+    });
+};
+
+getProducts();
